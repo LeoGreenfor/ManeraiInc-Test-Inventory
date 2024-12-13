ab1deca692fda5e9e1b37b049aa1a6ea91ea36a1
LeoGreenfor <97034848+LeoGreenfor@users.noreply.github.com>
2024-12-13 20:05:06 +0200

    basic drag controller

--- Inventory Test Task/Assets/Scripts/DragingController.cs
+++ Inventory Test Task/Assets/Scripts/DragingController.cs
@@ -2,7 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class DragingController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Camera mainCamera;
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Plane dragPlane; 
 
+    public float dragSpeed = 10f; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+
+        dragPlane = new Plane(mainCamera.transform.forward, transform.position);
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (dragPlane.Raycast(ray, out float distance))
+        {
+            Vector3 hitPoint = ray.GetPoint(distance);
+            offset = transform.position - hitPoint;
+        }
+
+        rb.useGravity = false;
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (dragPlane.Raycast(ray, out float distance))
+            {
+                Vector3 targetPosition = ray.GetPoint(distance) + offset;
+
+                Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * dragSpeed);
+                rb.MovePosition(newPosition);
+            }
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+
+        rb.useGravity = true;
+    }
 }
